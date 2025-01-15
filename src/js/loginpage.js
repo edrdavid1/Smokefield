d19cd2ba281018a0d6725569a323d5e6407ca94e
David Murashka <murashka.david@student.ehu.lt>
2025-01-15 22:33:17 +0200

    pwa v3

--- src/js/loginpage.js
+++ src/js/loginpage.js
@@ -27,3 +27,15 @@ document.getElementById("loginForm").addEventListener("submit", async function (
         alert('Error during login. Please try again.');
     }
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('../sw.js')
+            .then((registration) => {
+                console.log('Service Worker registered with scope: ', registration.scope);
+            })
+            .catch((error) => {
+                console.log('Service Worker registration failed: ', error);
+            });
+    });
+}
